46794bad127d1a9043d54c34a0368edf72287fd4
wilson bay <wilson.bay8@gmail.com>
2021-11-29 21:53:32 +0800

    added booking status to booking

--- db/migrate/20211129134615_create_bookings.rb
+++ db/migrate/20211129134615_create_bookings.rb
@@ -3,6 +3,7 @@ class CreateBookings < ActiveRecord::Migration[6.1]
     create_table :bookings do |t|
       t.integer :start_date
       t.integer :end_date
+      t.string :booking_status
 
       t.references :user, null: false, foreign_key: true
       t.references :listing, null: false, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_29_115900) do
+ActiveRecord::Schema.define(version: 2021_11_29_134615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_11_29_115900) do
   create_table "bookings", force: :cascade do |t|
     t.integer "start_date"
     t.integer "end_date"
+    t.string "booking_status"
     t.bigint "user_id", null: false
     t.bigint "listing_id", null: false
     t.datetime "created_at", precision: 6, null: false
