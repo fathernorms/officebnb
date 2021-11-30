887c98be35c75afd5013def5a40d156789d9c7f2
Norman Robert <normanrobertf@gmail.com>
2021-11-30 19:06:56 +0800

    updated user model & added profile photo display

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_094317) do
+ActiveRecord::Schema.define(version: 2021_11_30_100430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,11 @@ ActiveRecord::Schema.define(version: 2021_11_30_094317) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.text "description"
+    t.integer "owner_rating", default: 0
+    t.integer "host_rating", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
