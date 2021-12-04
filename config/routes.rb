710653460137fd3dd9709e40bcbe6518db07985d
Venga Elan <vengaelan@gmail.com>
2021-12-04 15:20:05 +0800

    added show method to bookings controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :listings do
-    resources :bookings, only: [ :new, :create ] do
+    resources :bookings, only: [ :new, :create, ] do
       resources :reviews, only: [ :new, :create ]
     end
   end
-  resources :bookings, only: [ :edit, :update ]
+  resources :bookings, only: [ :show, :edit, :update ]
   resources :reviews, only: [ :edit, :update, :destroy ]
   resources :users, only: :show
 end
