549d141f91da932a49245e193d1331e449af4813
Venga Elan <vengaelan@gmail.com>
2021-12-04 14:29:22 +0800

    Created users controller, show method and show.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :listings do
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   end
   resources :bookings, only: [ :edit, :update ]
   resources :reviews, only: [ :edit, :update, :destroy ]
+  resources :users, only: :show
 end
