252a3503a982aff236101ecec04a1456f876bd50
Venga Elan <vengaelan@gmail.com>
2021-12-06 20:17:44 +0800

    renamed date attr in bookings table and added new date attr of type date

--- db/migrate/20211205035134_change_dates_to_bookings.rb
+++
@@ -1,7 +0,0 @@
-class ChangeDatesToBookings < ActiveRecord::Migration[6.1]
-  def change
-    change_column :bookings, :start_date, :date
-    change_column :bookings, :end_date, :date
-  end
-  end
-end
---
+++ db/migrate/20211206121052_add_start_date_end_date_to_bookings.rb
@@ -0,0 +1,6 @@
+class AddStartDateEndDateToBookings < ActiveRecord::Migration[6.1]
+  def change
+      add_column :bookings, :start_date, :date
+      add_column :bookings, :end_date, :date
+  end
+end
