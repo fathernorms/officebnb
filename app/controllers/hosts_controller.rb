e32deaafd1219f877ef9d0a4c4d267117c4f4ab9
Norman Robert <normanrobertf@gmail.com>
2021-12-09 00:14:34 +0800

    fixed some minor issues

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,8 +1,10 @@
 class HostsController < ApplicationController
   def show
     @host = User.find(params[:id])
-    @upcoming = @host.listings.first.bookings.where(booking_status: ["PENDING", "CONFIRMED"])
-    @past = @host.listings.first.bookings.where(booking_status: ["COMPLETED", "CANCELLED"])
+    if @host.listings.length.positive?
+      @upcoming = @host.listings.first.bookings.where(booking_status: ["PENDING", "CONFIRMED"])
+      @past = @host.listings.first.bookings.where(booking_status: ["COMPLETED", "CANCELLED"])
+    end
   end
 
   def edit
