dc6fa9b336ae6b1f6ea431521ae7737f06d886fe
Norman Robert <normanrobertf@gmail.com>
2021-12-09 01:11:20 +0800

    added listing photo to index card

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,7 +5,7 @@ class Listing < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :address, presence: true
-  validates :description, presence: true, length: { maximum: 300 }
+  validates :description, presence: true, length: { maximum: 800 }
   validates :capacity, presence: true, numericality: { only_integer: true }
   validates :rate_per_day, presence: true, numericality: true
 
